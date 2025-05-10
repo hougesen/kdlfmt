9549e2b326ca3ffd9dd7c30ba340cf17b0bbd006
Mads Hougesen <mads@mhouge.dk>
2025-05-10 18:19:47 +0200

    test(cli): validate completions command (#121)

---
+++ cli/tests/main.rs
@@ -0,0 +1,88 @@
+#[cfg(test)]
+mod test_cli {
+    use predicates::prelude::PredicateBooleanExt;
+    use tempfile::tempdir;
+
+    fn kdlfmt_command(path: &std::path::Path) -> assert_cmd::Command {
+        let mut cmd = assert_cmd::Command::cargo_bin("kdlfmt").unwrap();
+
+        cmd.current_dir(path);
+
+        cmd
+    }
+
+    #[test]
+    fn help_arg_outputs_message() {
+        let dir = tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("--help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
+    }
+
+    #[test]
+    fn version_arg_outputs_message() {
+        let dir = tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("--version")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
+    }
+
+    mod help {
+        use predicates::prelude::PredicateBooleanExt;
+        use tempfile::tempdir;
+
+        use crate::test_cli::kdlfmt_command;
+
+        #[test]
+        fn command_outputs_help_message() {
+            let dir = tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("help")
+                .assert()
+                .success()
+                .stdout(predicates::str::is_empty().not());
+        }
+    }
+
+    mod completions {
+        use predicates::prelude::PredicateBooleanExt;
+        use tempfile::tempdir;
+
+        use crate::test_cli::kdlfmt_command;
+
+        #[test]
+        fn help_arg_outputs_message() {
+            let dir = tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("completions")
+                .arg("--help")
+                .assert()
+                .success()
+                .stdout(predicates::str::is_empty().not());
+        }
+
+        #[test]
+        fn outputs_shell_completions() {
+            let dir = tempdir().unwrap();
+
+            let shells = ["bash", "elvish", "fish", "powershell", "zsh"];
+
+            for shell in shells {
+                kdlfmt_command(dir.path())
+                    .arg("completions")
+                    .arg(shell)
+                    .assert()
+                    .success()
+                    .stdout(predicates::str::is_empty().not());
+            }
+        }
+    }
+}
