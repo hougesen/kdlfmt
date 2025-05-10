455c966008a171237f167068f3e214e607102ab2
Mads Hougesen <mads@mhouge.dk>
2025-05-10 18:57:18 +0200

    refactor(cli): make log level to global arg (#124)

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1,9 +1,4 @@
-use crate::{
-    cli::{Commands, LogLevel},
-    config::KdlFmtConfig,
-    error::KdlFmtError,
-    terminal::logging::setup_logger,
-};
+use crate::{cli::Commands, config::KdlFmtConfig, error::KdlFmtError};
 
 mod check;
 mod completions;
@@ -13,27 +8,13 @@ mod init;
 #[inline]
 pub fn execute_command(command: Commands) -> Result<(), KdlFmtError> {
     match command {
-        Commands::Check(args) => {
-            setup_logger(args.log_level.unwrap_or(LogLevel::Debug));
-
-            check::run(&args, &KdlFmtConfig::load()?)
-        }
-        Commands::Format(args) => {
-            setup_logger(args.log_level.unwrap_or(LogLevel::Debug));
-
-            format::run(&args, &KdlFmtConfig::load()?)
-        }
+        Commands::Check(args) => check::run(&args, &KdlFmtConfig::load()?),
         Commands::Completions(args) => {
             completions::run(&args, &mut std::io::stdout());
 
             Ok(())
         }
-        Commands::Init(args) => {
-            setup_logger(args.log_level.unwrap_or(LogLevel::Debug));
-
-            init::run(&args)?;
-
-            Ok(())
-        }
+        Commands::Format(args) => format::run(&args, &KdlFmtConfig::load()?),
+        Commands::Init(args) => init::run(&args),
     }
 }
