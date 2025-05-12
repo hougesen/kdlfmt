81a2281faca8cf4215110ca4ccae9a08b6c6073a
Mads Hougesen <mads@mhouge.dk>
2025-05-12 18:08:11 +0200

    test: --stdin arg with format and check

--- cli/tests/main.rs
+++ cli/tests/main.rs
@@ -1,58 +1,58 @@
-#[cfg(test)]
-mod test_cli {
-    use std::io::Write;
+use std::io::Write as _;
 
+const BROKEN_V1_CODE: &str = r#"world          {
+      child  "1"
+ child                "2"
+   }
+"#;
+
+const FORMATTED_V1_CODE: &str = r#"world {
+    child "1"
+    child "2"
+}
+"#;
+
+const BROKEN_V2_CODE: &str = r#"world          {
+      child  "1"
+ child                "2"
+   }
+"#;
+
+const FORMATTED_V2_CODE: &str = r#"world {
+    child "1"
+    child "2"
+}
+"#;
+
+fn kdlfmt_command(path: &std::path::Path) -> assert_cmd::Command {
+    let mut cmd = assert_cmd::Command::cargo_bin("kdlfmt").unwrap();
+
+    cmd.current_dir(path);
+
+    cmd
+}
+
+fn setup_test_input(dir: &std::path::Path, code: &str) -> tempfile::NamedTempFile {
+    let mut b = tempfile::Builder::new();
+
+    b.rand_bytes(12).suffix(".kdl");
+
+    let mut f = b.tempfile_in(dir).unwrap();
+
+    f.write_all(code.as_bytes()).unwrap();
+    f.flush().unwrap();
+
+    f
+}
+
+mod test_base_command {
     use predicates::prelude::PredicateBooleanExt;
-    use tempfile::tempdir;
 
-    const BROKEN_V1_CODE: &str = r#"world          {
-      child  "1"
- child                "2"
-   }
-"#;
-
-    const FORMATTED_V1_CODE: &str = r#"world {
-    child "1"
-    child "2"
-}
-"#;
-
-    const BROKEN_V2_CODE: &str = r#"world          {
-      child  "1"
- child                "2"
-   }
-"#;
-
-    const FORMATTED_V2_CODE: &str = r#"world {
-    child "1"
-    child "2"
-}
-"#;
-
-    fn kdlfmt_command(path: &std::path::Path) -> assert_cmd::Command {
-        let mut cmd = assert_cmd::Command::cargo_bin("kdlfmt").unwrap();
-
-        cmd.current_dir(path);
-
-        cmd
-    }
-
-    fn setup_test_input(dir: &std::path::Path, code: &str) -> tempfile::NamedTempFile {
-        let mut b = tempfile::Builder::new();
-
-        b.rand_bytes(12).suffix(".kdl");
-
-        let mut f = b.tempfile_in(dir).unwrap();
-
-        f.write_all(code.as_bytes()).unwrap();
-        f.flush().unwrap();
-
-        f
-    }
+    use crate::kdlfmt_command;
 
     #[test]
     fn help_arg_outputs_message() {
-        let dir = tempdir().unwrap();
+        let dir = tempfile::tempdir().unwrap();
 
         kdlfmt_command(dir.path())
             .arg("--help")
@@ -63,7 +63,7 @@ mod test_cli {
 
     #[test]
     fn version_arg_outputs_message() {
-        let dir = tempdir().unwrap();
+        let dir = tempfile::tempdir().unwrap();
 
         kdlfmt_command(dir.path())
             .arg("--version")
@@ -71,138 +71,143 @@ mod test_cli {
             .success()
             .stdout(predicates::str::is_empty().not());
     }
+}
 
-    mod help {
-        use predicates::prelude::PredicateBooleanExt;
-        use tempfile::tempdir;
+#[cfg(test)]
+mod test_help_command {
+    use predicates::prelude::PredicateBooleanExt;
 
-        use crate::test_cli::kdlfmt_command;
+    use crate::kdlfmt_command;
 
-        #[test]
-        fn command_outputs_help_message() {
-            let dir = tempdir().unwrap();
+    #[test]
+    fn command_outputs_help_message() {
+        let dir = tempfile::tempdir().unwrap();
 
-            kdlfmt_command(dir.path())
-                .arg("help")
-                .assert()
-                .success()
-                .stdout(predicates::str::is_empty().not());
-        }
+        kdlfmt_command(dir.path())
+            .arg("help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
+    }
+}
+
+#[cfg(test)]
+mod test_completions_command {
+    use predicates::prelude::PredicateBooleanExt;
+
+    use crate::kdlfmt_command;
+
+    #[test]
+    fn help_arg_outputs_message() {
+        let dir = tempfile::tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("completions")
+            .arg("--help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
     }
 
-    mod completions {
-        use predicates::prelude::PredicateBooleanExt;
-        use tempfile::tempdir;
+    #[test]
+    fn outputs_shell_completions() {
+        let dir = tempfile::tempdir().unwrap();
 
-        use crate::test_cli::kdlfmt_command;
+        let shells = ["bash", "elvish", "fish", "nushell", "powershell", "zsh"];
 
-        #[test]
-        fn help_arg_outputs_message() {
-            let dir = tempdir().unwrap();
-
+        for shell in shells {
             kdlfmt_command(dir.path())
                 .arg("completions")
-                .arg("--help")
+                .arg(shell)
                 .assert()
                 .success()
                 .stdout(predicates::str::is_empty().not());
         }
+    }
+}
 
-        #[test]
-        fn outputs_shell_completions() {
-            let dir = tempdir().unwrap();
+#[cfg(test)]
+mod test_init_command {
+    use predicates::prelude::PredicateBooleanExt;
 
-            let shells = ["bash", "elvish", "fish", "nushell", "powershell", "zsh"];
+    use crate::kdlfmt_command;
 
-            for shell in shells {
-                kdlfmt_command(dir.path())
-                    .arg("completions")
-                    .arg(shell)
-                    .assert()
-                    .success()
-                    .stdout(predicates::str::is_empty().not());
-            }
-        }
+    #[test]
+    fn help_arg_outputs_message() {
+        let dir = tempfile::tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("init")
+            .arg("--help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
     }
 
-    mod init {
-        use predicates::prelude::PredicateBooleanExt;
-        use tempfile::tempdir;
+    #[test]
+    fn creates_a_config_file() {
+        let dir = tempfile::tempdir().unwrap();
 
-        use super::kdlfmt_command;
+        kdlfmt_command(dir.path()).arg("init").assert().success();
 
-        #[test]
-        fn help_arg_outputs_message() {
-            let dir = tempdir().unwrap();
+        let config_file_created = dir.path().join("kdlfmt.kdl").try_exists().unwrap();
 
-            kdlfmt_command(dir.path())
-                .arg("init")
-                .arg("--help")
-                .assert()
-                .success()
-                .stdout(predicates::str::is_empty().not());
-        }
-
-        #[test]
-        fn creates_a_config_file() {
-            let dir = tempdir().unwrap();
-
-            kdlfmt_command(dir.path()).arg("init").assert().success();
-
-            let config_file_created = dir.path().join("kdlfmt.kdl").try_exists().unwrap();
-
-            assert!(config_file_created);
-        }
-
-        #[test]
-        fn fails_if_config_exists() {
-            let dir = tempdir().unwrap();
-
-            kdlfmt_command(dir.path()).arg("init").assert().success();
-
-            kdlfmt_command(dir.path()).arg("init").assert().failure();
-        }
-
-        #[test]
-        fn force_config_arg() {
-            let dir = tempdir().unwrap();
-
-            kdlfmt_command(dir.path()).arg("init").assert().success();
-
-            kdlfmt_command(dir.path()).arg("init").assert().failure();
-
-            kdlfmt_command(dir.path())
-                .arg("init")
-                .arg("--force")
-                .assert()
-                .success();
-        }
+        assert!(config_file_created);
     }
 
-    mod format {
-        use predicates::prelude::PredicateBooleanExt;
-        use tempfile::tempdir;
+    #[test]
+    fn fails_if_config_exists() {
+        let dir = tempfile::tempdir().unwrap();
 
-        use super::kdlfmt_command;
-        use crate::test_cli::{
-            BROKEN_V1_CODE, BROKEN_V2_CODE, FORMATTED_V1_CODE, FORMATTED_V2_CODE, setup_test_input,
+        kdlfmt_command(dir.path()).arg("init").assert().success();
+
+        kdlfmt_command(dir.path()).arg("init").assert().failure();
+    }
+
+    #[test]
+    fn force_config_arg() {
+        let dir = tempfile::tempdir().unwrap();
+
+        kdlfmt_command(dir.path()).arg("init").assert().success();
+
+        kdlfmt_command(dir.path()).arg("init").assert().failure();
+
+        kdlfmt_command(dir.path())
+            .arg("init")
+            .arg("--force")
+            .assert()
+            .success();
+    }
+}
+
+#[cfg(test)]
+mod test_format_command {
+    use predicates::prelude::PredicateBooleanExt;
+
+    use crate::kdlfmt_command;
+
+    #[test]
+    fn help_arg_outputs_message() {
+        let dir = tempfile::tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("format")
+            .arg("--help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
+    }
+
+    mod auto {
+
+        use crate::{
+            BROKEN_V1_CODE, BROKEN_V2_CODE, FORMATTED_V1_CODE, FORMATTED_V2_CODE, kdlfmt_command,
+            setup_test_input,
         };
 
         #[test]
-        fn help_arg_outputs_message() {
-            let dir = tempdir().unwrap();
-
-            kdlfmt_command(dir.path())
-                .arg("format")
-                .arg("--help")
-                .assert()
-                .success()
-                .stdout(predicates::str::is_empty().not());
-        }
-
-        #[test]
         fn formats_broken_code() {
-            let dir = tempdir().unwrap();
+            let dir = tempfile::tempdir().unwrap();
 
             {
                 let file = setup_test_input(dir.path(), BROKEN_V1_CODE);
@@ -234,8 +239,37 @@ mod test_cli {
         }
 
         #[test]
-        fn formats_broken_code_kdl_version_v1() {
-            let dir = tempdir().unwrap();
+        fn formats_broken_code_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            {
+                kdlfmt_command(dir.path())
+                    .arg("format")
+                    .arg("--stdin")
+                    .write_stdin(BROKEN_V1_CODE)
+                    .assert()
+                    .success()
+                    .stdout(predicates::str::contains(FORMATTED_V1_CODE));
+            };
+
+            {
+                kdlfmt_command(dir.path())
+                    .arg("format")
+                    .arg("--stdin")
+                    .write_stdin(BROKEN_V2_CODE)
+                    .assert()
+                    .success()
+                    .stdout(predicates::str::contains(FORMATTED_V2_CODE));
+            }
+        }
+    }
+
+    mod v1 {
+        use crate::{BROKEN_V1_CODE, FORMATTED_V1_CODE, kdlfmt_command, setup_test_input};
+
+        #[test]
+        fn formats_broken_code() {
+            let dir = tempfile::tempdir().unwrap();
 
             let file = setup_test_input(dir.path(), BROKEN_V1_CODE);
 
@@ -253,8 +287,27 @@ mod test_cli {
         }
 
         #[test]
-        fn formats_broken_code_kdl_version_v2() {
-            let dir = tempdir().unwrap();
+        fn formats_broken_code_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("format")
+                .arg("--kdl-version")
+                .arg("v1")
+                .arg("--stdin")
+                .write_stdin(BROKEN_V1_CODE)
+                .assert()
+                .success()
+                .stdout(predicates::str::contains(FORMATTED_V1_CODE));
+        }
+    }
+
+    mod v2 {
+        use crate::{BROKEN_V2_CODE, FORMATTED_V2_CODE, kdlfmt_command, setup_test_input};
+
+        #[test]
+        fn formats_broken_code() {
+            let dir = tempfile::tempdir().unwrap();
 
             let file = setup_test_input(dir.path(), BROKEN_V2_CODE);
 
@@ -270,32 +323,51 @@ mod test_cli {
 
             assert_eq!(output, FORMATTED_V2_CODE);
         }
+
+        #[test]
+        fn formats_broken_code_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("format")
+                .arg("--kdl-version")
+                .arg("v2")
+                .arg("--stdin")
+                .write_stdin(BROKEN_V2_CODE)
+                .assert()
+                .success()
+                .stdout(predicates::str::contains(FORMATTED_V2_CODE));
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_check_command {
+    use predicates::prelude::PredicateBooleanExt;
+
+    use crate::kdlfmt_command;
+
+    #[test]
+    fn help_arg_outputs_message() {
+        let dir = tempfile::tempdir().unwrap();
+
+        kdlfmt_command(dir.path())
+            .arg("check")
+            .arg("--help")
+            .assert()
+            .success()
+            .stdout(predicates::str::is_empty().not());
     }
 
-    mod check {
-        use predicates::prelude::PredicateBooleanExt;
-        use tempfile::tempdir;
-
-        use super::{
+    mod auto {
+        use crate::{
             BROKEN_V1_CODE, BROKEN_V2_CODE, FORMATTED_V1_CODE, FORMATTED_V2_CODE, kdlfmt_command,
             setup_test_input,
         };
 
         #[test]
-        fn help_arg_outputs_message() {
-            let dir = tempdir().unwrap();
-
-            kdlfmt_command(dir.path())
-                .arg("check")
-                .arg("--help")
-                .assert()
-                .success()
-                .stdout(predicates::str::is_empty().not());
-        }
-
-        #[test]
         fn success_with_formatted_input() {
-            let dir = tempdir().unwrap();
+            let dir = tempfile::tempdir().unwrap();
 
             {
                 let file = setup_test_input(dir.path(), FORMATTED_V1_CODE);
@@ -319,43 +391,31 @@ mod test_cli {
         }
 
         #[test]
-        fn success_with_formatted_input_kdl_version_v1() {
-            let dir = tempdir().unwrap();
+        fn success_with_formatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
 
-            let file = setup_test_input(dir.path(), FORMATTED_V1_CODE);
+            {
+                kdlfmt_command(dir.path())
+                    .arg("check")
+                    .arg("--stdin")
+                    .write_stdin(FORMATTED_V1_CODE)
+                    .assert()
+                    .success();
+            };
 
-            kdlfmt_command(dir.path())
-                .arg("check")
-                .arg("--kdl-version")
-                .arg("v1")
-                .arg(file.path())
-                .assert()
-                .success();
+            {
+                kdlfmt_command(dir.path())
+                    .arg("check")
+                    .arg("--stdin")
+                    .write_stdin(FORMATTED_V2_CODE)
+                    .assert()
+                    .success();
+            };
         }
-
-        #[test]
-        fn success_with_formatted_input_kdl_version_v2() {
-            let dir = tempdir().unwrap();
-
-            let file = setup_test_input(dir.path(), FORMATTED_V2_CODE);
-
-            kdlfmt_command(dir.path())
-                .arg("check")
-                .arg("--kdl-version")
-                .arg("v2")
-                .arg(file.path())
-                .assert()
-                .success();
-        }
-
-        ///////
-        ///
-        ///
-        ///
 
         #[test]
         fn fail_with_unformatted_input() {
-            let dir = tempdir().unwrap();
+            let dir = tempfile::tempdir().unwrap();
 
             {
                 let file = setup_test_input(dir.path(), BROKEN_V1_CODE);
@@ -379,8 +439,64 @@ mod test_cli {
         }
 
         #[test]
-        fn fail_with_unformatted_kdl_version_v1() {
-            let dir = tempdir().unwrap();
+        fn fail_with_unformatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            {
+                kdlfmt_command(dir.path())
+                    .arg("check")
+                    .arg("--stdin")
+                    .write_stdin(BROKEN_V1_CODE)
+                    .assert()
+                    .failure();
+            };
+
+            {
+                kdlfmt_command(dir.path())
+                    .arg("check")
+                    .arg("--stdin")
+                    .write_stdin(BROKEN_V2_CODE)
+                    .assert()
+                    .failure();
+            };
+        }
+    }
+
+    mod v1 {
+        use crate::{BROKEN_V1_CODE, FORMATTED_V1_CODE, kdlfmt_command, setup_test_input};
+
+        #[test]
+        fn success_with_formatted_input() {
+            let dir = tempfile::tempdir().unwrap();
+
+            let file = setup_test_input(dir.path(), FORMATTED_V1_CODE);
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v1")
+                .arg(file.path())
+                .assert()
+                .success();
+        }
+
+        #[test]
+        fn success_with_formatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v1")
+                .arg("--stdin")
+                .write_stdin(FORMATTED_V1_CODE)
+                .assert()
+                .success();
+        }
+
+        #[test]
+        fn fail_with_unformatted_input() {
+            let dir = tempfile::tempdir().unwrap();
 
             let file = setup_test_input(dir.path(), BROKEN_V1_CODE);
 
@@ -394,8 +510,55 @@ mod test_cli {
         }
 
         #[test]
-        fn fail_with_unformatted_kdl_version_v2() {
-            let dir = tempdir().unwrap();
+        fn fail_with_unformatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v1")
+                .arg("--stdin")
+                .write_stdin(BROKEN_V1_CODE)
+                .assert()
+                .failure();
+        }
+    }
+
+    mod v2 {
+        use crate::{BROKEN_V2_CODE, FORMATTED_V2_CODE, kdlfmt_command, setup_test_input};
+
+        #[test]
+        fn success_with_formatted_input() {
+            let dir = tempfile::tempdir().unwrap();
+
+            let file = setup_test_input(dir.path(), FORMATTED_V2_CODE);
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v2")
+                .arg(file.path())
+                .assert()
+                .success();
+        }
+
+        #[test]
+        fn success_with_formatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v2")
+                .arg("--stdin")
+                .write_stdin(FORMATTED_V2_CODE)
+                .assert()
+                .success();
+        }
+
+        #[test]
+        fn fail_with_unformatted_input() {
+            let dir = tempfile::tempdir().unwrap();
 
             let file = setup_test_input(dir.path(), BROKEN_V2_CODE);
 
@@ -404,6 +567,20 @@ mod test_cli {
                 .arg("--kdl-version")
                 .arg("v2")
                 .arg(file.path())
+                .assert()
+                .failure();
+        }
+
+        #[test]
+        fn fail_with_unformatted_input_stdin() {
+            let dir = tempfile::tempdir().unwrap();
+
+            kdlfmt_command(dir.path())
+                .arg("check")
+                .arg("--kdl-version")
+                .arg("v2")
+                .arg("--stdin")
+                .write_stdin(BROKEN_V2_CODE)
                 .assert()
                 .failure();
         }
